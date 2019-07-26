afcf95f32c1ea0aac66dff41e2e7d6a113cabe95
Brian Guo <BGuo@Brians-Macbook.local>
2019-07-26 15:33:33 -0400

    brian

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello welcome to Tariq's website
+          Hello welcome to Brian's website
         </p>
         <a
           className="App-link"
